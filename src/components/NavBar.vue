11461ec009858ee25ca678b99b6c32221408b92d
Matheus-Ayres <matheussayres9@gmail.com>
2026-06-01 15:22:28 -0300

    Nav feito e responsivo :iphone::sparkles:

---
+++ src/components/NavBar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import SearchBar from './SearchBar.vue';
+
+
+</script>
+
+<template>
+    <nav class="bg-gray-800 h-15 p-5 flex flex-col gap-8">
+        <div class="flex justify-between items-center
+            lg:grid grid-cols-[auto_1fr_auto_auto] gap-6
+        ">
+
+            <div class="flex gap-3 items-center">
+                <img src="../assets/pokeball-icon.svg" class="w-12 h-12">
+                <span class="text-white font-bold text-xl">PokéAPI Explorer</span>
+            </div>
+
+            <SearchBar class="hidden 
+            lg:flex justify-self-center w-full max-w-xl
+            " />
+
+            <div class="hidden lg:flex gap-1 items-center group cursor-pointer">
+                <div class="relative w-5 h-5">
+                    <img src="../assets/favs.png" class="absolute inset-0 group-hover:hidden">
+
+                    <img src="../assets/liked.png" class="absolute inset-0 hidden group-hover:block">
+                </div>
+
+                <span class="text-white">Favoritos</span>
+            </div>
+
+            <img src="../assets/red-icon.jpeg" class="w-14 h-14 rounded-full">
+
+
+        </div>
+        <SearchBar class="lg:hidden" />
+    </nav>
+</template>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="bg-gray-700 border border-gray-500 rounded-md p-2 flex gap-2 items-center w-full
+        lg:w-full
+    ">
+
+        <img src="../assets/search.png" class="w-8">
+
+        <input type="text" placeholder="Buscar Pokémon por nome ou número" 
+        class="bg-gray-700 text-white focus:outline-none rounded-md p-2 w-full">
+        
+    </div>
+</template>
