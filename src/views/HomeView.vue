5728aaea85a40bc55cca06d365013184ab39e61d
dalpozzo <eduardodpp07@gmail.com>
2026-06-11 14:48:34 -0300

    Arrumando o a view do card aberto

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,32 @@
 <script setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
+import { RouterView, useRoute } from 'vue-router'
+
 import AreaCards from '../components/AreaCards.vue'
 import Generation from '../components/Generation.vue'
 import NavBar from '../components/NavBar.vue'
 import PokeTypesDesktop from '../components/PokeTypesDesktop.vue'
 import PokeTypesMobile from '../components/PokeTypesMobile.vue'
-import { RouterView } from 'vue-router'
+
+const route = useRoute()
 
 const selectedTypes = ref([])
 const selectedGeneration = ref(null)
 const searchQuery = ref('')
+
+const pokemonIsOpen = computed(() => {
+    return Boolean(route.params.id)
+})
 </script>
 
 <template>
     <NavBar v-model:search-query="searchQuery" />
 
     <div class="p-5">
-        <main class="lg:grid lg:grid-cols-[20%_50%_30%] gap-4">
+        <main
+            class="grid grid-cols-1 gap-4 lg:grid-cols-[20%_minmax(0,1fr)_30%]"
+        >
+            <!-- Filtros no mobile -->
             <div class="mb-4 flex flex-col gap-4 lg:hidden">
                 <PokeTypesMobile
                     v-model:selected-types="selectedTypes"
@@ -29,6 +39,7 @@ const searchQuery = ref('')
                 </div>
             </div>
 
+            <!-- Filtros à esquerda no desktop -->
             <aside
                 class="hidden rounded-xl bg-white p-4 lg:flex lg:flex-col lg:gap-4"
             >
@@ -41,13 +52,23 @@ const searchQuery = ref('')
                 />
             </aside>
 
+            <!-- Cards no centro -->
             <AreaCards
-                class="lg:col-span-2"
+                :class="{
+                    'lg:col-span-2': !pokemonIsOpen
+                }"
                 :selected-types="selectedTypes"
                 :selected-generation="selectedGeneration"
                 :search-query="searchQuery"
             />
-            <RouterView/>
+
+            <!-- Detalhes à direita -->
+            <aside
+                v-if="pokemonIsOpen"
+                class="min-w-0 self-start lg:sticky lg:top-5"
+            >
+                <RouterView />
+            </aside>
         </main>
     </div>
 </template>
